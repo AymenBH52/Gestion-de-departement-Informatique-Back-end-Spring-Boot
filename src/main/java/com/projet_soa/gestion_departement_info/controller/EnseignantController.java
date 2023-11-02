38622644fb9610108abaf1508305893207841bdd
mouna bedoui <mounabedoui6@gmail.com>
2023-11-02 21:35:50 +0100

    commit

--- src/main/java/com/projet_soa/gestion_departement_info/controller/EnseignantController.java
+++ src/main/java/com/projet_soa/gestion_departement_info/controller/EnseignantController.java
@@ -1,6 +1,7 @@
 package com.projet_soa.gestion_departement_info.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,35 +14,35 @@ import com.projet_soa.gestion_departement_info.entities.Enseignant;
 
 import java.util.List;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/enseignants")
 public class EnseignantController {
 
     @Autowired
     private EnseignantService enseignantService;
-   
+
     @GetMapping
     public List<Enseignant> getAllEnseignants() {
         return enseignantService.getAllEnseignants();
-    }    
-  
+    }
+
     @GetMapping("/{id}")
     public Enseignant getEnseignantById(Long id) {
         return enseignantService.getEnseignantById(id);
     }
 
-   @PostMapping("/{id}")
+    @PostMapping("/{id}")
     public Enseignant saveEnseignant(Enseignant enseignant) {
         return enseignantService.saveEnseignant(enseignant);
     }
 
-   @PutMapping("/{id}")
+    @PutMapping("/{id}")
     public Enseignant updateEnseignant(Long id, Enseignant enseignant) {
         return enseignantService.updateEnseignant(id, enseignant);
     }
 
-   @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}")
     public void deleteEnseignantById(Long id) {
         enseignantService.deleteEnseignantById(id);
     }
