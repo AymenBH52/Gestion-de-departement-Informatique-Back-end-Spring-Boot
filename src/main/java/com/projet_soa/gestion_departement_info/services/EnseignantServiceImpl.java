fb12ac74a929feb514fa81fb3c45cdaaf4b76b4e
mouna bedoui <mounabedoui6@gmail.com>
2023-11-03 17:06:16 +0100

    commit

--- src/main/java/com/projet_soa/gestion_departement_info/services/EnseignantServiceImpl.java
+++ src/main/java/com/projet_soa/gestion_departement_info/services/EnseignantServiceImpl.java
@@ -1,6 +1,7 @@
 package com.projet_soa.gestion_departement_info.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,9 +23,22 @@ public class EnseignantServiceImpl implements EnseignantService {
         return enseignantRepository.findById(id).get();
     }
 
-    public Enseignant updateEnseignant(Long id,  Enseignant enseignant) {
-        enseignant.setId(id);
-        return enseignantRepository.save(enseignant);
+    public Enseignant updateEnseignant(Long id, Enseignant enseignant) {
+        Optional<Enseignant> existingEnseignant = enseignantRepository.findById(id);
+        if (existingEnseignant.isPresent()) {
+            Enseignant updatedEnseignant = existingEnseignant.get();
+            updatedEnseignant.setNom(enseignant.getNom());
+            updatedEnseignant.setPrenom(enseignant.getPrenom());
+            updatedEnseignant.setEmail(enseignant.getEmail());
+            updatedEnseignant.setTelephone(enseignant.getTelephone());
+            updatedEnseignant.setAdresse(enseignant.getAdresse());
+            updatedEnseignant.setSexe(enseignant.getSexe());
+            updatedEnseignant.setGrade(enseignant.getGrade());
+            updatedEnseignant.setSpecialite(enseignant.getSpecialite());
+            return enseignantRepository.save(updatedEnseignant);
+        } else {
+            return null;
+        }
     }
 
     @Override
